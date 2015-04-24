a2a337545f5593b51fe2edee47eb768f526ec9f6
Quanlong He <kyan.ql.he@gmail.com>
2015-04-24 08:53:36 +0800

    Use cache images when coreos-install

--- app/config/cloud-config-bootstrap.sh
+++ app/config/cloud-config-bootstrap.sh
@@ -8,6 +8,6 @@ set -e
 until curl -O http://%(server_ip)s/cloud-config.yml; do sleep 2; done
 
 # Install coreos
-until sudo coreos-install -d /dev/sda -c cloud-config.yml; do sleep 2; done
+until sudo coreos-install -d /dev/sda -c cloud-config.yml -b http://%(server_ip)s:3000; do sleep 2; done
 
 sudo reboot
